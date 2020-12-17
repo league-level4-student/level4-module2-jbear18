2ecc10402dc9ff5e6ceecd44d80fc99efefe6748
jbear18 <shehu01@hotmail.com>
2020-12-16 19:59:34 -0800

    Stringmethods IP

--- src/StringMethods/StringMethods.java
+++ src/StringMethods/StringMethods.java
@@ -3,7 +3,6 @@ package StringMethods;
 import java.util.Arrays;
 import java.util.Base64;
 
-
 /*
 Visit the JavaDocs for the String class to view everything you can do with a String.  
 
@@ -31,59 +30,95 @@ Character.getNumericValue(char c);
 
 public class StringMethods {
 
-
 	// Given Strings s1 and s2, return the longer String
 	public static String longerString(String s1, String s2) {
-		
-		int sOne= s1.length();
-		int sTwo= s2.length();
-	if( sOne > sTwo) {
-		return s1;
-	}
-	if(sTwo>sOne) {
-		return s2;
-	}
+
+		int sOne = s1.length();
+		int sTwo = s2.length();
+		if (sOne > sTwo) {
+			return s1;
+		}
+		if (sTwo > sOne) {
+			return s2;
+		}
 		return "equal";
 	}
 
-	
-	// if String s contains the word "underscores", change all of the spaces to underscores
+	// if String s contains the word "underscores", change all of the spaces to
+	// underscores
 	public static String formatSpaces(String s) {
-		if(s.contains("underscores")) {
-		s=s.replace(" ", "_");
-		
-		return s;
-		} 
+		if (s.contains("underscores")) {
+			s = s.replace(" ", "_");
 
-			return s;	
+			return s;
+		}
+
+		return s;
 	}
 
-	
-	// Return the name of the person whose LAST name would appear first if they were in alphabetical order
+	// Return the name of the person whose LAST name would appear first if they were
+	// in alphabetical order
 	// You cannot assume there are no extra spaces around the name, but you can
 	// assume there is only one space between the first and last name
 	public static String lineLeader(String s1, String s2, String s3) {
+		int c12 = s1.compareToIgnoreCase(s2);
+		int c23 = s2.compareToIgnoreCase(s3);
+		int c13 = s1.compareToIgnoreCase(s3);
+
+		s1 = s1.trim();
+		s2 = s2.trim();
+		s3 = s3.trim();
+
+		String last1 = s1.split(" ")[1];
+
+		String last2 = s2.split(" ")[1];
+
+		String last3 = s3.split(" ")[1];
+
+		if (last1.compareTo(last2) < 0 && last1.compareTo(last3) < 0) {
+			return s1;
+		} else if (last2.compareTo(last1) < 0 && last2.compareTo(last3) < 0) {
+			return s2;
+		} else if (last3.compareTo(last2) < 0 && last3.compareTo(last1) < 0) {
+			return s3;
+		}
+
 		return null;
 	}
-	
-	
+
 	// Return the sum of all numerical digits in the String
 	public static int numeralSum(String s) {
-		return 0;
+
+		int sum = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (Character.isDigit(s.charAt(i))) {
+			String temp= s.substring(i, i+1);
+			sum += Integer.parseInt(temp);
+			} 
+		}
+		return sum;
 	}
-	
-	
+
+
 	// Return the number of times String substring appears in String s
 	public static int substringCount(String s, String substring) {
-		int appear=0;
-		for (int i = 0; i < s.length(); i++) {
-			if(s.contains(substring)) {
-				appear++;
-				return appear;
-			}
-		}
-	
-		return 0;
+int string= s.length();
+int times= substring.length();
+
+int temp=0;
+for (int i = 0; i <= times-string; i++) {
+for (int j = 0; j < string; j++) {
+	if(substring.charAt(i+j)!=s.charAt(j)) {
+		break;
+	}
+	if(j==string) {
+		temp++;
+		j=0;
+	}
+}
+}
+
+		return temp;
 	}
 
 	// Call Utitilities.encrypt to encrypt String s
@@ -96,13 +131,11 @@ public class StringMethods {
 		return null;
 	}
 
-
 	// Return the number of words in String s that end with String substring
 	// You can assume there are no punctuation marks between words
 	public static int wordsEndsWithSubstring(String s, String substring) {
 		return 0;
 	}
-	
 
 	// Given String s, return the number of characters between the first occurrence
 	// of String substring and the final occurrence
@@ -111,14 +144,13 @@ public class StringMethods {
 		return 0;
 	}
 
-
 	// Return true if String s is a palindrome
 	// palindromes are words or phrases are read the same forward as backward.
 	// HINT: ignore/remove all punctuation and spaces in the String
 	public static boolean palindrome(String s) {
 		return true;
 	}
-	
+
 }
 
 class Utilities {
